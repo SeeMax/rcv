60f7e3e4ce853c195993f9c1d6bed2d1ef775ccb
Max Mearsheimer <mmearshe@gmail.com>
2012-01-11 14:24:38 -0600

    fixed up spacing of letters in the entry box. worked on spacng of border in gif div. will have to further fix gif then make spacing correct again. fixed word replacement since it was replacing parts of some words.

--- cnc.rb
+++ cnc.rb
@@ -10,15 +10,18 @@ post '/' do
   @name = params['name']
   @name = @name.titleize.strip.squeeze(" ")
 
-  @name.gsub!("the","")
-  @name.gsub!("The","")
-  @name.gsub!("a ","")
-  @name.gsub!("A ","")
-  @name.gsub!("company","")
-  @name.gsub!("Company","")
-  @name.gsub!("We","")
-  @name.gsub!("we","")
+  @name.gsub!(" the "," ")
+  @name.gsub!("The "," ")
+  @name.gsub!(" a "," ")
+  @name.gsub!("A "," ")
+  @name.gsub!(" company "," ")
+  @name.gsub!(" Company "," ")
+  @name.gsub!("We "," ")
+  @name.gsub!(" we "," ")
 
   @name = @name + " Company"
   erb :name
 end
+
+
+
